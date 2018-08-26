9383bb5d893ba604316411ba5f4daa9cb867eaec
Andressa Tonietto <andressatreis@gmail.com>
2018-08-26 14:27:02 -0300

    Skip error message test

--- src/parseReverseString/parseReverseString.js
+++ src/parseReverseString/parseReverseString.js
@@ -1,8 +1,9 @@
 const parserReverse = (param) => {
   let reverse = '';
   if (typeof param === 'number') {
-    return new Error(['Param type must be a string']);
+    throw new Error('Param type must be a string');
   }
+
   for (let i = param.length - 1; i >= 0; i--) {
     /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
     reverse += param[i];
