128cea4c7fcb33b2fe4f74ac856d4df4cc4a7402
Andressa Tonietto <andressatreis@gmail.com>
2018-08-26 16:16:31 -0300

    Update index tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,10 @@
 /* global test expect */
 import parser from '.'; // NOTE: when file name is missing, the importer will look for index.js
 
-test('should have a function to parseBoolean', () => {
+test('Should have a function to parseBoolean', () => {
   expect(parser.parseBoolean(0)).toBe(false);
+});
+
+test('Should have a function for parseInt', () => {
+  expect(parser.parseInteger(1)).toBe(1);
 });
